df09653439c9bba40b32b0dafcafc8b041eb6107
farisubuntu <farisubuntu@gmail.com>
2025-12-20 23:08:25 +0300

    update...

---
+++ mind-maps/AI Development.mm
@@ -0,0 +1,161 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="AI Development" FOLDED="false" ID="ID_1824574276" CREATED="1766254292862" MODIFIED="1766261026526" STYLE="oval" BORDER_COLOR_LIKE_EDGE="false">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.464">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<edge COLOR="#000000"/>
+<node TEXT="&#xd83d;&#xdcc3; Temlates&#xd83d;&#xdee3;&#xfe0f;" POSITION="right" ID="ID_124646120" CREATED="1766254322523" MODIFIED="1766261018332" HGAP_QUANTITY="1.250000379979582 pt" VSHIFT_QUANTITY="176.24999474734082 pt" COLOR="#ff3366">
+<node ID="ID_685741569" CREATED="1766260817936" MODIFIED="1766261230630" HGAP_QUANTITY="10.250000111758702 pt" VSHIFT_QUANTITY="56.24999832361942 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Examples:<i>&#160;<font color="#0000c0">Chatbot Starter, RAG (Knowledge Base) Template</font></i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1483805089" CREATED="1766260819447" MODIFIED="1766261138701" HGAP_QUANTITY="8.750000156462193 pt" VSHIFT_QUANTITY="-59.999998211860714 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <b><i>&#55357;&#56715;&#65039;&#160;&#160;Pre-built starter kits provided by Vercel to help you skip the &quot;boilerplate&quot; setup (auth, DB, styling).</i></b>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="&#xd83e;&#xdde0; Models &#xd83d;&#xde92;" POSITION="left" ID="ID_1457485790" CREATED="1766254302953" MODIFIED="1766261063527" HGAP_QUANTITY="-6.249999396502972 pt" VSHIFT_QUANTITY="109.4999967366458 pt" COLOR="#ff3366">
+<edge COLOR="#7c0000"/>
+<node ID="ID_1859013211" CREATED="1766260817936" MODIFIED="1766261078848" HGAP_QUANTITY="10.250000111758702 pt" VSHIFT_QUANTITY="56.24999832361942 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Examples:<i>&#160;<font color="#0000c0">gpt-4o, claude-3-5-sonnet, gemini-1.5-pro</font></i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1212694874" CREATED="1766260819447" MODIFIED="1766261054334" HGAP_QUANTITY="8.750000156462193 pt" VSHIFT_QUANTITY="-59.999998211860714 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <b><i>&#55357;&#56715;&#65039; The actual Large Language Model (LLM) that processes text and generates logic.</i></b>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="&#xd83d;&#xdc66; Providers &#x26fd;" POSITION="right" ID="ID_372066073" CREATED="1766254316873" MODIFIED="1766261026525" HGAP_QUANTITY="-139.74999541789308 pt" VSHIFT_QUANTITY="-55.499998345971164 pt" COLOR="#ff3366">
+<edge COLOR="#00007c"/>
+<node ID="ID_502656342" CREATED="1766260817936" MODIFIED="1766261221092" HGAP_QUANTITY="10.250000111758702 pt" VSHIFT_QUANTITY="56.24999832361942 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Examples:<i>&#160;<font color="#0000c0">OpenAI, Anthropic, Google, AWS Bedrock</font>&#160;</i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1034665444" CREATED="1766260819447" MODIFIED="1766261127544" HGAP_QUANTITY="8.750000156462193 pt" VSHIFT_QUANTITY="-59.999998211860714 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <b><i>&#55357;&#56715;&#65039; The service that provides an API to access those models. They handle the billing and infrastructure.</i></b>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</map>
